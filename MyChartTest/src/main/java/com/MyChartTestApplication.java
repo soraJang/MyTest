708fb1a25e3c56712a59b8c335a800ab38149b89
skyso <skyso@DESKTOP-T0T817B>
2018-07-17 13:18:57 +0900

    commit

---
+++ MyChartTest/src/main/java/com/MyChartTestApplication.java
@@ -0,0 +1,12 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyChartTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyChartTestApplication.class, args);
+	}
+}
