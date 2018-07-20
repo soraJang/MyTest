d4f29b728e00d5726ec0a0d4632c12d386861f66
skysora <skysora@DESKTOP-T0T817B>
2018-07-20 14:06:11 +0900

    default setting

---
+++ SmartCity/src/main/java/com/ctr/TestController.java
@@ -0,0 +1,18 @@
+package com.ctr;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TestController {
+
+	@RequestMapping("/main")
+	public String main() {
+		return "main";
+	}
+	
+	@RequestMapping("/index")
+	public String index() {
+		return "index";
+	}
+}
